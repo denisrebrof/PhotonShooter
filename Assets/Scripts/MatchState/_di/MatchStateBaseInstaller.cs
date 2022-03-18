5664bd04fed359169a7994ee02be7fbc7fe3955a
Denis Rebrov <denisrebrof@gmail.com>
2022-03-18 14:07:19 +0300

    vvvvv

---
+++ Assets/Scripts/MatchState/_di/MatchStateBaseInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using MatchState.data;
+using MatchState.domain;
+using MatchState.domain.repositories;
+using UnityEngine;
+using Zenject;
+
+namespace MatchState._di
+{
+    [CreateAssetMenu(fileName = "MatchStateBaseInstaller", menuName = "Installers/MatchStateBaseInstaller", order = 0)]
+    public class MatchStateBaseInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private MatchTimersDurationScriptableObjectRepository timersSORepository;
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<IMatchStateRepository>().To<MatchStateInMemoryRepository>().AsSingle();
+            Container.Bind<IMatchTimersDurationRepository>().FromInstance(timersSORepository).AsSingle();
+            Container.Bind<IMatchTimerRepository>().To<MatchTimerInMemoryRepository>().AsSingle();
+        }
+    }
+}
---
+++ Assets/Scripts/MatchState/domain/StartMatchStateUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MatchState.domain.model;
+using UniRx;
+using Zenject;
+
+namespace MatchState.domain
+{
+    public class SetMatchStateUseCase
+    {
+        [Inject] private MatchStateDurationUseCase matchStateDurationUseCase;
+        [Inject] private IMatchStateRepository matchStateRepository;
+        [Inject] private IMatchTimerRepository matchTimerRepository;
+
+        public void SetMatchState(MatchStates state)
+        {
+            var currentState = matchStateRepository.GetMatchState();
+            if(currentState == state) return;
+            
+            matchStateRepository.SetMatchState(state);
+            matchTimerRepository.StopTimer();
+            if (!matchStateDurationUseCase.GetStateDurationIfItHas(state, out var duration)) ;
+            matchTimerRepository.StartTimer(duration);
+        }
+    }
+}
